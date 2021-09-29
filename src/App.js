5386d236388dba704e2d76b4ed845964d2122298
Hrishikesh Bolinjkar <bolinjkar.rishi@outlook.com>
2021-09-29 13:01:16 +0530

    toggle func to open/close sidebar on small screen

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import "./_app.scss";
 import Header from "./components/header/Header";
 import Sidebar from "./components/sidebar/Sidebar";
 import HomeScreen from "./screens/homescreen/HomeScreen";
+import { useState } from "react";
 
 function App() {
+  const [toggleSidebar, setToggleSidebar] = useState(false);
+
+  const handleToggleSidebar = () => setToggleSidebar(!toggleSidebar);
+
   return (
     <>
-      <Header />
+      <Header handleToggleSidebar={handleToggleSidebar} />
       <div className="app__container">
-        <Sidebar />
+        <Sidebar
+          toggleSidebar={toggleSidebar}
+          handleToggleSidebar={handleToggleSidebar}
+        />
         <Container fluid className="app__main">
           <HomeScreen />
         </Container>
