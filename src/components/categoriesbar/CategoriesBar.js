5dc37309ca8326d31cc68c0bbe8fc5c162d46058
Hrishikesh Bolinjkar <bolinjkar.rishi@outlook.com>
2021-09-29 23:21:23 +0530

    feat: added categories and video component to home

--- src/components/categoriesbar/CategoriesBar.js
+++ src/components/categoriesbar/CategoriesBar.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import "./_categoriesbar.scss";
 
+const keywords = [
+  "All",
+  "JavaScript",
+  "Computer Programming",
+  "Crypto",
+  "Football",
+  "Marty Shwartz",
+  "Hip Hop",
+  "Gym",
+  "Guitar",
+  "Podcast",
+  "React JS",
+  "Psychology",
+];
 const CategoriesBar = () => {
-  return <div className="categories">Categories</div>;
+  const [active, setActive] = useState("All");
+  return (
+    <div className="categoriesbar">
+      {keywords.map((value, i) => (
+        <span
+          key={i}
+          onClick={() => setActive(value)}
+          className={active === value ? "active" : ""}
+        >
+          {value}
+        </span>
+      ))}
+    </div>
+  );
 };
 
 export default CategoriesBar;
--- src/components/video/Video.js
+++ src/components/video/Video.js
@@ -1,8 +1,39 @@
 import React from "react";
 import "./_video.scss";
+import { AiFillEye } from "react-icons/ai";
 
 const Video = () => {
-  return <div className="video">Video</div>;
+  return (
+    <div className="video">
+      <div className="video__top">
+        <img
+          src="https://i.ytimg.com/vi/rt-2cxAiPJk/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD5FHa8UXdC3VA95b5HmLxam1S7kQ"
+          alt="Video Preview"
+        />
+        <span>05:43</span>
+      </div>
+      <div className="video__description">
+        <img
+          src="https://yt3.ggpht.com/fGvQjp1vAT1R4bAKTFLaSbdsfdYFDwAzVjeRVQeikH22bvHWsGULZdwIkpZXktcXZc5gFJuA3w=s176-c-k-c0x00ffffff-no-rj-mo"
+          alt="channel"
+        />
+        <div className="details">
+          <div className="details__title">
+            SPIDER-MAN: NO WAY HOME - Official Teaser Trailer (HD)
+          </div>
+          <div className="details__channel">
+            <span>Marvel Entertainment</span>
+          </div>
+          <div className="details__stats">
+            <span>
+              <AiFillEye /> 5m Views •
+            </span>
+            <span> 5 days ago</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Video;
