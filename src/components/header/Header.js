bf486f35e159ed4f266cd27e08bb4b01442356ef
Hrishikesh Bolinjkar <bolinjkar.rishi@outlook.com>
2021-09-29 13:00:12 +0530

    feat: added styled sidebar and header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,30 @@
 import React from "react";
 import "./_header.scss";
+import { FaBars } from "react-icons/fa";
+import { AiOutlineSearch } from "react-icons/ai";
+import { MdApps, MdNotifications, MdAccountCircle } from "react-icons/md";
 
-const Header = () => {
-  return <div className="header">Header</div>;
+const Header = ({ handleToggleSidebar }) => {
+  return (
+    <div className="header">
+      <FaBars
+        className="header__menu"
+        size={26}
+        onClick={handleToggleSidebar}
+      />
+      <img src="/images/youtube_logo.png" alt="logo" className="header__logo" />
+      <form>
+        <input type="text" placeholder="Search" />
+        <button>
+          <AiOutlineSearch size={22} color="white" />
+        </button>
+      </form>
+      <div className="header__icons">
+        <MdApps size={28} />
+        <MdNotifications size={28} />
+        <MdAccountCircle size={28} />
+      </div>
+    </div>
+  );
 };
-
 export default Header;
--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,8 +1,33 @@
 import React from "react";
 import "./_sidebar.scss";
+import {
+  MdHome,
+  MdSubscriptions,
+  MdThumbUp,
+  MdExplore,
+  MdHistory,
+  MdExitToApp,
+} from "react-icons/md";
 
-const Sidebar = () => {
-  return <div className="sidebar">Sidebar</div>;
+const Sidebar = ({ toggleSidebar, handleToggleSidebar }) => {
+  const component = (icon, title) => (
+    <li>
+      {icon}
+      <span>{title}</span>
+    </li>
+  );
+  return (
+    <nav className={toggleSidebar ? "sidebar open" : "sidebar"}>
+      {component(<MdHome size={23} />, "Home")}
+      {component(<MdSubscriptions size={23} />, "Subscriptions")}
+      {component(<MdThumbUp size={23} />, "Liked")}
+      {component(<MdExplore size={23} />, "Explore")}
+      {component(<MdHistory size={23} />, "History")}
+      <hr />
+      {component(<MdExitToApp size={23} />, "Log out")}
+      <hr />
+    </nav>
+  );
 };
 
 export default Sidebar;
