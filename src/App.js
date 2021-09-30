e7c262ff42d16bcf4c73c947b421384af2cbcead
Hrishikesh Bolinjkar <bolinjkar.rishi@outlook.com>
2021-09-30 15:46:53 +0530

    react router setup

--- src/App.js
+++ src/App.js
@@ -2,27 +2,43 @@ import { Container } from "react-bootstrap";
 import "./_app.scss";
 import Header from "./components/header/Header";
 import Sidebar from "./components/sidebar/Sidebar";
-import HomeScreen from "./screens/homescreen/HomeScreen";
+import HomeScreen from "./screens/HomeScreen/HomeScreen";
 import { useState } from "react";
+import LoginScreen from "./screens/LoginScreen/LoginScreen";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
-  const [toggleSidebar, setToggleSidebar] = useState(false);
+  const Layout = ({ children }) => {
+    const [toggleSidebar, setToggleSidebar] = useState(false);
+    const handleToggleSidebar = () => setToggleSidebar(!toggleSidebar);
 
-  const handleToggleSidebar = () => setToggleSidebar(!toggleSidebar);
+    return (
+      <>
+        <Header handleToggleSidebar={handleToggleSidebar} />
+        <div className="app__container">
+          <Sidebar
+            toggleSidebar={toggleSidebar}
+            handleToggleSidebar={handleToggleSidebar}
+          />
+          <Container fluid className="app__main">
+            {children}
+          </Container>
+        </div>
+      </>
+    );
+  };
 
   return (
-    <>
-      <Header handleToggleSidebar={handleToggleSidebar} />
-      <div className="app__container">
-        <Sidebar
-          toggleSidebar={toggleSidebar}
-          handleToggleSidebar={handleToggleSidebar}
-        />
-        <Container fluid className="app__main">
-          <HomeScreen />
-        </Container>
-      </div>
-    </>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Layout>
+            <HomeScreen />
+          </Layout>
+        </Route>
+        <Route path="/auth" component={LoginScreen} />
+      </Switch>
+    </Router>
   );
 }
 
