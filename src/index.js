71a4318ed3168fd63ac9f951c72aa0551ec699bd
Hrishikesh Bolinjkar <bolinjkar.rishi@outlook.com>
2021-10-01 01:46:47 +0530

    setup redux store, action creators and providers

--- src/index.js
+++ src/index.js
@@ -3,5 +3,23 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./_base.scss";
+import { applyMiddleware, createStore } from "redux";
+import { authReducer } from "./store/reducers/authReducer";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(
+  authReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
