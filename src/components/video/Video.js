7933e9c240cf89f1f8d74cf34426607333fc6082
Hrishikesh Bolinjkar <bolinjkar.rishi@outlook.com>
2021-10-04 17:41:35 +0530

    dispatching actions to get vid deets and render

--- src/components/video/Video.js
+++ src/components/video/Video.js
@@ -1,34 +1,78 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./_video.scss";
 import { AiFillEye } from "react-icons/ai";
+import moment from "moment";
+import numeral from "numeral";
+import request from "../../api";
 
-const Video = () => {
+const Video = ({ video }) => {
+  const {
+    id,
+    snippet: {
+      channelId,
+      title,
+      channelTitle,
+      publishedAt,
+      thumbnails: { medium },
+    },
+  } = video;
+
+  const [views, setViews] = useState(null);
+  const [duration, setDuration] = useState(null);
+  const [channelIcon, setChannelIcon] = useState(null);
+
+  const seconds = moment.duration(duration).asSeconds();
+  const videoDuration = moment.utc(seconds * 1000).format("mm:ss");
+
+  useEffect(() => {
+    const get_video_details = async () => {
+      const {
+        data: { items },
+      } = await request("/videos", {
+        params: {
+          part: "contentDetails,statistics",
+          id: id,
+        },
+      });
+      setViews(items[0].statistics.viewCount); //in all these cases items[0] is unnecessary but required for the youtube api to work
+      setDuration(items[0].contentDetails.duration);
+    };
+    get_video_details();
+  }, [id]);
+
+  useEffect(() => {
+    const get_channel_details = async () => {
+      const {
+        data: { items },
+      } = await request.get("/channels", {
+        params: {
+          part: "snippet",
+          id: channelId,
+        },
+      });
+      setChannelIcon(items[0].snippet.thumbnails.default);
+    };
+    get_channel_details();
+  }, [channelId]);
+
   return (
     <div className="video">
       <div className="video__top">
-        <img
-          src="https://i.ytimg.com/vi/rt-2cxAiPJk/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD5FHa8UXdC3VA95b5HmLxam1S7kQ"
-          alt="Video Preview"
-        />
-        <span>05:43</span>
+        <img src={medium.url} alt="Video Preview" />
+        <span>{videoDuration}</span>
       </div>
       <div className="video__description">
-        <img
-          src="https://yt3.ggpht.com/fGvQjp1vAT1R4bAKTFLaSbdsfdYFDwAzVjeRVQeikH22bvHWsGULZdwIkpZXktcXZc5gFJuA3w=s176-c-k-c0x00ffffff-no-rj-mo"
-          alt="channel"
-        />
+        <img src={channelIcon?.url} alt="channel" />
         <div className="details">
-          <div className="details__title">
-            SPIDER-MAN: NO WAY HOME - Official Teaser Trailer (HD)
-          </div>
+          <div className="details__title">{title}</div>
           <div className="details__channel">
-            <span>Marvel Entertainment</span>
+            <span>{channelTitle}</span>
           </div>
           <div className="details__stats">
             <span>
-              <AiFillEye /> 5m Views •
+              <AiFillEye /> {numeral(views).format("0.a")} Views •
             </span>
-            <span> 5 days ago</span>
+            <span> {moment(publishedAt).fromNow()} </span>
           </div>
         </div>
       </div>
