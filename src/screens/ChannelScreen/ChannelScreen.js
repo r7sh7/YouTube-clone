d478d1fe6a89b658553970f9b19bab89cad0e7b1
Hrishikesh Bolinjkar <bolinjkar.rishi@gmail.com>
2021-12-16 14:43:40 +0530

    feat: channel screen done

--- src/screens/ChannelScreen/ChannelScreen.js
+++ src/screens/ChannelScreen/ChannelScreen.js
@@ -1,34 +1,56 @@
 import React, { useEffect } from "react";
+import "./channelScreen.scss";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { useParams } from "react-router";
 import { getVideosByChannel } from "../../store/actions/videoActions";
 import { Col, Container, Row } from "react-bootstrap";
 import Video from "../../components/video/Video";
+import { getChannelDetails } from "../../store/actions/channelActions";
+import numeral from "numeral";
 
 const ChannelScreen = ({ setActive }) => {
   const dispatch = useDispatch();
   const { channelId } = useParams();
   const { videos, loading } = useSelector((state) => state.channelVideos);
+  const { snippet, statistics } = useSelector(
+    (state) => state.channelDetails.channel
+  );
   useEffect(() => {
     dispatch(getVideosByChannel(channelId));
+    dispatch(getChannelDetails(channelId));
   }, [dispatch, channelId]);
   useEffect(() => {
     setActive("");
   });
 
   return (
-    <Container>
-      {!loading && (
-        <Row>
-          {videos.map((video) => (
-            <Col lg={3} md={4}>
-              <Video video={video} key={video.id} channelVideos />
-            </Col>
-          ))}
-        </Row>
-      )}
-    </Container>
+    <>
+      <div className="channelHeader">
+        <div className="channelHeader__main">
+          <img src={snippet?.thumbnails?.default?.url} alt="" />
+          <div>
+            <h3>{snippet?.title}</h3>
+            <span>
+              {numeral(statistics?.subscriberCount).format("0.a")} subscribers
+            </span>
+          </div>
+        </div>
+        <button>SUBSCRIBED</button>
+      </div>
+      <hr />
+      <Container>
+        {!loading && (
+          <Row>
+            {videos.map((video) => (
+              <Col lg={3} md={4}>
+                <Video video={video} key={video.id} channelVideos />
+              </Col>
+            ))}
+          </Row>
+        )}
+      </Container>
+    </>
   );
 };
 
