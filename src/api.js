0c6e2eb65252f1004daf1df9260641276ea69b5b
Hrishikesh Bolinjkar <bolinjkar.rishi@gmail.com>
2022-01-27 12:49:37 +0530

    API key uodate

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const request = axios.create({
   baseURL: "https://youtube.googleapis.com/youtube/v3/",
   params: {
-    key: "AIzaSyAX77tiRcG8CTHobRBy8W37PIM80RwsL88",
+    key: "AIzaSyBFXj3sK6Q2geD-48mGSLQHzewhmKuYSiU",
   },
 });
 
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,7 +1,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAX77tiRcG8CTHobRBy8W37PIM80RwsL88",
+  apiKey: "AIzaSyBFXj3sK6Q2geD-48mGSLQHzewhmKuYSiU",
   authDomain: "yt-clone-1788f.firebaseapp.com",
   projectId: "yt-clone-1788f",
   storageBucket: "yt-clone-1788f.appspot.com",
